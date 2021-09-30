e7ccbe13c2032bcacbea953ef40128e547b8ab32
Ladislav Beganyi <sales@justclicktofile.com>
2021-09-30 18:34:00 -0400

    Update practice-area-hero.js
    update for push

--- src/components/practice-area-hero.js
+++ src/components/practice-area-hero.js
@@ -105,7 +105,7 @@ const PracticeAreaHero = ({ title, content }) => {
               href="#"
               className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
             >
-              Live demo
+              Contact Us
             </a>
           </div>
         </div>
