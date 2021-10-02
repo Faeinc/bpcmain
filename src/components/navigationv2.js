f842bda2b843852d92279e70b9e15f02a1a4f0db
Ladislav Beganyi <sales@justclicktofile.com>
2021-10-02 11:59:53 -0400

    added contact form

--- src/components/navigationv2.js
+++ src/components/navigationv2.js
@@ -191,7 +191,7 @@ const Navigation = () => {
             <a href="/blog" className="text-base font-medium text-gray-500 hover:text-gray-900">
               Blog
             </a>
-            <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
+            <a href="/contact" className="text-base font-medium text-gray-500 hover:text-gray-900">
               Contact Us
             </a>
           </Popover.Group>
