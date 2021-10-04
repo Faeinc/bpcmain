9baf1f59bfa0b60706e1e3d87d38db61f595452b
Ladislav Beganyi <sales@justclicktofile.com>
2021-10-03 23:47:36 -0400

    delete post

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -198,7 +198,7 @@ class Contact extends React.Component {
                 {/* Contact form */}
                 <div className="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12">
                   <h3 className="text-lg font-medium text-gray-900">Send us a message</h3>
-                  <form method="POST" data-netlify="true" name="Contact" className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
+                  <form data-netlify="true" name="Contact" className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
                     <div>
                       <label htmlFor="first-name" className="block text-sm font-medium text-gray-900">
                         First name
