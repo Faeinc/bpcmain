08c6c64c07248846c31728300975f45c5ccd2e8c
Ladislav Beganyi <sales@justclicktofile.com>
2021-10-05 02:51:59 -0400

    transforming code

--- src/components/article-content.js
+++ src/components/article-content.js
@@ -1,4 +1,12 @@
 import React from 'react'
+import ReactHtmlParser, { convertNodeToElement } from 'react-html-parser'
+function transformItem(node, index) {
+  // convert <ul> to <ol>
+  if (node.type === 'h2') {
+    node.type = 'ol';
+   // return convertNodeToElement(node, index, transform);
+  }
+}
 export default function ArticleContent({ post }) {
   const text = post.body.childMarkdownRemark.html
   const split = text.split(' ');
@@ -6,16 +14,40 @@ export default function ArticleContent({ post }) {
   const topHalf  = Math.ceil((split.length/2))
   const firstHalfOfArray = split.slice(0, bottomHalf).join(" ") + "</p>"
   const secondHalfOfArray = "<p>"+ split.slice(split.length-(topHalf)).join(" ")
+  const html = `
+  <p id="main">
+    <span class="prettify">
+      keep me and make me pretty!
+    </span>
+  </p>
+`;
+  let parse = ReactHtmlParser(html)
+  let parse2 = ReactHtmlParser(firstHalfOfArray);
+   parse2.forEach(
+    function(item, index, arr) {
+
+      if (item.type === 'h2') {
+        arr[index] = React.createElement('h3', {className: 'mt-6 prose prose-indigo leading-6 text-indigo-600 font-semibold tracking-wide uppercase', children:item.props.children}, )
+
+      }
+      else{
+        arr[index] = item;
+      }
+    }
+  )
+  console.log(parse)
+console.log(parse2)
+
+
 
   return (
     <div className="py-16 xl:py-36 px-4 sm:px-6 lg:px-8 bg-white overflow-hidden">
       <div className="max-w-max lg:max-w-7xl mx-auto">
         <div className="relative z-10 mb-8 md:mb-2 md:px-6">
           <div className="text-base max-w-prose lg:max-w-none">
-            <h2 className="leading-6 text-indigo-600 font-semibold tracking-wide uppercase">{post.title}</h2>
-            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-              A better way to send money
-            </p>
+            <h2 className="uppercase text-indigo-600 mt-2 text-2xl leading-8 font-semibold tracking-wide text-gray-900 sm:text-1xl">
+              {post.title}
+            </h2>
           </div>
         </div>
         <div className="relative">
@@ -69,8 +101,9 @@ export default function ArticleContent({ post }) {
                 <div dangerouslySetInnerHTML={{
                   __html: firstHalfOfArray
                 }} className="text-justify" />
-
-
+                {/*<div>
+                {parse2}
+              </div>*/}
               </div>
               <div className="mt-6 prose prose-indigo prose-lg text-gray-500 lg:mt-0">
                 <div dangerouslySetInnerHTML={{
