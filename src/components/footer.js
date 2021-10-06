b4a95cdf7d211a95554bee8c97e986bd7b83fe32
Ladislav Beganyi <sales@justclicktofile.com>
2021-10-06 10:10:35 -0400

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -162,14 +162,14 @@ const Footer = () => (
         </div>
       </div>
       <div className="mt-12 border-t border-gray-600 pt-8 md:flex md:items-center md:justify-between lg:mt-16">
-        <div className="flex space-x-6 md:order-2">
+       {/* <div className="flex space-x-6 md:order-2">
           {footerNavigation.social.map((item) => (
             <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
               <span className="sr-only">{item.name}</span>
               <item.icon className="h-6 w-6" aria-hidden="true" />
             </a>
           ))}
-        </div>
+        </div>*/}
         <p className="mt-8 text-base text-gray-600 md:mt-0 md:order-1">
           &copy; 2021 Beganyi Professional Corporation. All rights reserved.
         </p>
