cd3e763e94c1cd136984ba9a1c4716fa97a54508
Ladislav Beganyi <sales@justclicktofile.com>
2021-10-11 20:53:20 -0400

    Update residential-testimonial.js

--- src/components/residential/residential-testimonial.js
+++ src/components/residential/residential-testimonial.js
@@ -27,6 +27,7 @@ export default function ResidentialTestimonial() {
 
 
   return (
+    <>
     <section className="bg-white overflow-hidden">
       <div className="relative max-w-7xl mx-auto pt-20 pb-12 px-4 sm:px-6 lg:px-8 lg:py-20">
         <svg
@@ -127,5 +128,24 @@ export default function ResidentialTestimonial() {
         </div>
       </div>
     </section>
+      <div className="bg-gray-50">
+        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            <span className="block">Buying or Selling?</span>
+            <span className="block text-indigo-600">Hire Us</span>
+          </h2>
+          <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+            <div className="inline-flex rounded-md shadow">
+              <a
+                href="/contact/"
+                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+              >
+                We're here to help
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
