7966cc92b40bd59ca11cf37e45c4e5c8f0c3387f
Ladislav Beganyi <sales@justclicktofile.com>
2021-10-16 23:43:25 -0400

    add business law base

--- src/components/business/business-header.js
+++ src/components/business/business-header.js
@@ -12,14 +12,35 @@ export default function BusinessHeader() {
         <div className="flex flex-col justify-center w-full md:w-1/2 px-4 md:pr-12">
           <p className="uppercase text-lg text-gray-600 leading-normal">Business Law</p>
           <h2 className="text-4xl lg:text-5xl font-extrabold leading-tight text-gray-800 pt-4">
-            Fixed Rate Fees<br />
-            For Residential Transactions
+           Business Advice You Can Trust
           </h2>
           <p className="text-lg text-gray-600 leading-normal pt-4">
-            Whether you are a first time home buyer or an experienced residential real estate investor, we can assist you with closing your residential purchase, sale and refinance transactions. We represent individuals, families and businesses in all kinds of
-            residential real estate matters.
+            If you’re looking for a law firm that offers personalized service and
+            practical advice to businesses of all sizes, then Beganyi Professional Corporation
+            Law Firm is the right choice for you.
           </p>
-
+          <p className="text-lg text-gray-600 leading-normal pt-4">
+            We pride ourselves on providing our clients with individual attention.
+            By retaining us as your business lawyers, we will provide you with knowledge,
+            experience, and capabilities equal to much larger corporate-commercial law firms.
+          </p>
+          <p className="text-lg text-gray-600 leading-normal pt-4">
+            You can trust that when it comes time to make important decisions about
+            your company’s future or legal matters affecting your business, we will be there by
+            your side every step of the way. Our team understands how stressful these
+            situations can be so we strive to help ease some of this stress by offering
+            practical advice in plain language so you know what options are available to you at each stage of the process. We want nothing more than for our clients to feel comfortable knowing they have an experienced professional working hard on their behalf. This is why we offer free consultations where one of our professionals will sit down with you face-to-face and discuss any questions or concerns regarding your business. Let us put years worth of experience behind protecting your interests today!
+          </p>
+          <div className="mt-8 flex">
+            <div className="ml-3 inline-flex">
+              <a
+                href="/contact/"
+                className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+              >
+                Get Business Advice
+              </a>
+            </div>
+          </div>
         </div>
         <div className="bg-gray-900 w-full md:w-1/2 px-4 md:px-12 py-10 mt-8 md:mt-0 rounded mr-4">
           <div className="flex pb-10">
@@ -32,74 +53,88 @@ export default function BusinessHeader() {
             </div>
             <div className="mt-1">
               <h4 className="text-lg font-bold leading-tight text-white">Business Formation</h4>
-              <p className="text-base text-white leading-normal pt-2">If you’re buying a home or condo,
-                you’ll need a real estate lawyer to represent you.
-                A law firm with extensive experience in residential real estate purchases
-                is invaluable to minimize your risk and stress levels with one of life’s biggest
-                financial commitments. Let Beganyi Professional Corporation Law Firm help you
-                with your next home purchase.</p>
-            </div>
-          </div>
-          <div className="flex pb-10">
-            <div className="mr-5 text-white">
-              <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" />
-                <circle cx={12} cy={12} r={9} />
-                <path d="M9 12l2 2l4 -4" />
-              </svg>
-            </div>
-            <div className="mt-1">
-              <h4 className="text-lg font-bold leading-tight text-white">Corporate Governance</h4>
-              <p className="text-base text-white leading-normal pt-2">In a hot market like Toronto,
-                or Mississauga selling a home can be quite exciting.
-                If you’ve owned your home for a number of years, you’ve likely generated quite a
-                bit of profit. However, before you can watch your bank balance grow, you
-                need to make sure that your home sells successfully. Listing a home and
-                selling it through the agent is just the first step. You will need a
-                real estate lawyer to close the transaction and to help you deal with any
-                issues or concerns that are brought up by the purchaser’s lawyer.
-                Our firm can help you navigate this process. Let Beganyi Professional
-                Corporation Law Firm help you with your next home sale.</p>
-            </div>
-          </div>
-          <div className="flex pb-10">
-            <div className="mr-5 text-white">
-              <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" />
-                <circle cx={12} cy={12} r={9} />
-                <path d="M9 12l2 2l4 -4" />
-              </svg>
-            </div>
-            <div className="mt-1">
-              <h4 className="text-lg font-bold leading-tight text-white">Tax Structuring</h4>
-              <p className="text-base text-white leading-normal pt-2">At Beganyi P.C., we understand that real estate
-                can be a complicated process with many different aspects to consider
-                for both individuals and businesses looking to mortgage or refinance a property.
-                Ladislav is skilled at guiding clients through each step, so they have the best
-                opportunity possible while protecting their interests throughout this journey.</p>
-                <p className="text-base text-white leading-normal pt-2">
-                The law firm provides individualized attention no matter what your reason
-                  for refinancing your existing residential mortgage. Whether you're securing
-                  funds for improvements on your home, paying off debts, or just taking advantage of
-                  more beneficial interest rates offered by banks these days, we are here to help.
-              </p>
-            </div>
-            <div className="mt-1">
-              <h4 className="text-lg font-bold leading-tight text-white">Equity & Debt Financing</h4>
-              <p className="text-base text-white leading-normal pt-2">At Beganyi P.C., we understand that real estate
-                can be a complicated process with many different aspects to consider
-                for both individuals and businesses looking to mortgage or refinance a property.
-                Ladislav is skilled at guiding clients through each step, so they have the best
-                opportunity possible while protecting their interests throughout this journey.</p>
               <p className="text-base text-white leading-normal pt-2">
-                The law firm provides individualized attention no matter what your reason
-                for refinancing your existing residential mortgage. Whether you're securing
-                funds for improvements on your home, paying off debts, or just taking advantage of
-                more beneficial interest rates offered by banks these days, we are here to help.
+                If you're a business owner, one of the first things you should do is figure
+                out what legal structure your business should have.
+              </p>
+              <p className="text-base text-white leading-normal pt-2">
+                There are many structures available including: sole proprietorship,
+                general partnership, limited partnership and corporation. Each has its own
+                set of pros and cons. A good lawyer can help new entrepreneurs figure out which
+                type of business structure will best suit their needs.
+              </p>
+              <p className="text-base text-white leading-normal pt-2">
+                Beganyi Professional Corporation Law Firm specializes in helping business
+                owners choose the right kind of entity that fits their needs with minimal
+                hassle or cost to them. We offer affordable rates for all clients regardless
+                if they are startups or established businesses.
+                Their goal is to make sure that every client's concerns are addressed
+                quickly and efficiently so they don't need to worry about anything else but
+                running their business effectively.
               </p>
             </div>
           </div>
-          <div className="flex">
+          <div className="flex pb-10">
+            <div className="mr-5 text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" />
+                <circle cx={12} cy={12} r={9} />
+                <path d="M9 12l2 2l4 -4" />
+              </svg>
+            </div>
+            <div className="mt-1">
+              <h4 className="text-lg font-bold leading-tight text-white">Corporate Reorganizations</h4>
+              <p className="text-base text-white leading-normal pt-2">
+                If you are looking to reorganize your business, it is important to work
+                with an experienced lawyer who understands the various options available
+                and can help you navigate through this process.
+              </p>
+              <p className="text-base text-white leading-normal pt-2">
+                The choice of structure for a corporate reorganization is very important
+                because there are many tax and operational consequences that will affect
+                your company in the future. Many companies choose to use an existing
+                corporation as part of their restructuring plan, but there may be better
+                alternatives depending on your situation. It is important to work with a
+                team of professionals including accountants and financial advisers if you
+                decide to restructure your company's operations or ownership structure.
+              </p>
+              <p className="text-base text-white leading-normal pt-2">
+                Ladislav Beganyi has years of experience advising clients on all types of
+                corporate restructurings. He combines his legal background with his masters
+                in tax to deliver targeted and profitable solutions to his clients.
+              </p>
+            </div>
+
+          </div>
+          <div className="flex pb-10">
+            <div className="mr-5 text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" />
+                <circle cx={12} cy={12} r={9} />
+                <path d="M9 12l2 2l4 -4" />
+              </svg>
+            </div>
+            <div className="mt-1">
+              <h4 className="text-lg font-bold leading-tight text-white">Contract Drafting & Review</h4>
+              <p className="text-base text-white leading-normal pt-2">
+                When you're running a business, contracts and other agreements
+                are an inevitable part of doing business. You need to have them
+                reviewed by a professional before signing anything.
+              </p>
+              <p className="text-base text-white leading-normal pt-2">
+                But who do you trust? Who is qualified enough to review your
+                contracts and agreements?
+              </p>
+              <p className="text-base text-white leading-normal pt-2">
+                Beganyi Professional Corporation Law Firm has the experience that you need in
+                order to make sure that your contract or agreement has been thoroughly
+                reviewed for any potential issues. This way,
+                there won't be any surprises when it comes time for signing. Let us help!
+              </p>
+            </div>
+          </div>
+
+         {/* <div className="flex">
             <div className="mr-5 text-white">
               <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" />
@@ -121,7 +156,7 @@ export default function BusinessHeader() {
                 please contact us.
               </p>
             </div>
-          </div>
+          </div>*/}
         </div>
       </div>
     </section>
